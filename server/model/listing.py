ffd2fbbbbaf640a65b8cd23db7c74293646d26e8
cl8295 <christy.lim28@gmail.com>
2024-02-07 15:27:29 +0800

    update listing

--- server/model/listing.py
+++ server/model/listing.py
@@ -1,5 +1,6 @@
-import os
+
 from model.DatabasePool import DatabasePool
+import traceback
 
 class Listing:
     @classmethod 
@@ -19,5 +20,29 @@ class Listing:
 
             return recsChanged
         
+        finally:
+            dbConn.close()
+            
+    @classmethod
+    def updateListing(cls, new_title, new_description, new_price, new_country, new_travelperiod, new_imageURL, travelID):
+        try:
+            dbConn = DatabasePool.getConnection()
+            db_Info = dbConn.connection_id
+            print(f"Connected to {db_Info}")
+            cursor = dbConn.cursor(dictionary=True)
+
+            # update travel listing in the database
+            sql = "UPDATE listing SET title=%s, description=%s, price=%s, country=%s, travel_period=%s, imageURL=%s WHERE travelID=%s"
+            cursor.execute(sql, (new_title, new_description, new_price, new_country, new_travelperiod, new_imageURL, travelID))
+
+            dbConn.commit()
+
+            return {"message": "Information updated successfully", "status": "success"}
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            traceback.print_exc()
+            return {"message": f"Error updating data: {str(e)}", "status": "error"}
+
         finally:
             dbConn.close()
